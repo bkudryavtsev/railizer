ba1234d0999e1adfb6e02d8f3a1472d308a1b0ad
Boris Kudryavtsev <boriskk.work@gmail.com>
2021-04-01 17:58:16 -0700

    Add date range to slider, fix margins

--- src/charts.js
+++ src/charts.js
@@ -2,31 +2,35 @@ import * as d3 from 'd3';
 
 export class MainChart {
   constructor(width, height) {
-    this.margin = { top: 0, right: 0, bottom: 0, left: 0 };
-    this.width = width - this.margin.left - this.margin.right;
-    this.height = height - this.margin.top - this.margin.bottom;
-    this.locationWidth = 120;
-    this.defaultBarWidth = 2000;
+    this.margin = { top: 0, right: 100, bottom: 0, left: 100 };
+    this.centerTextWidth = 100;
+    this.defaultBarWidth = 100;
+    this.width = width;
+    this.height = height;
+    this.center = this.width / 2;
 
-    this.x = d3.scaleLinear()
+    this.x1 = d3.scaleLinear()
       .domain([0, this.defaultBarWidth])
-      .range([0, this.width / 2]);
+      .range([0, this.center - this.centerTextWidth / 2 - this.margin.left]);
+
+    this.x2 = d3.scaleLinear()
+      .domain([0, this.defaultBarWidth])
+      .range([0, this.center - this.centerTextWidth / 2 - this.margin.right]);
 
     this.y = d3.scaleBand()
       .rangeRound([0, this.height])
-      .padding(0.1)
+      .padding(0.1);
 
-    d3.select('#main-chart').selectAll('svg').remove()
+    d3.select('#main-chart').selectAll('svg').remove();
 
     this.svg = d3.select('#main-chart').append('svg')
-      .attr('width', this.width + this.margin.left + this.margin.right)
-      .attr('height', this.height + this.margin.top + this.margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+      .attr("viewBox", [0, 0, this.width, this.height])
+      .append('g');
   }
 
   update(data) {
-    this.x.domain([0, d3.max(data, d => d['INFRAINVEST'])]);
+    this.x1.domain([0, d3.max(data, d => d['ROADACCID'])]);
+    this.x2.domain([0, d3.max(data, d => d['INFRAINVEST'])]);
     this.y.domain(data.map(d => d['LOCATION']));
 
     const row = this.svg.selectAll('g.row')
@@ -35,19 +39,26 @@ export class MainChart {
     const newRow = row.enter()
       .append('g')
       .attr('class', 'row')
-      .attr('transform', `translate(0, ${this.height + this.margin.top + this.margin.bottom})`);
+      .attr('transform', `translate(0, ${this.height})`);
 
     newRow.insert('rect')
-      .attr('class','bar')
-      .attr('x', this.locationWidth)
+      .attr('class','bar-roadaccid')
+      .attr('x', d => this.center - this.centerTextWidth / 2 - this.x1(d['ROADACCID']))
       .attr('opacity', 0)
       .attr('height', this.y.bandwidth())
-      .attr('width', d => this.x(d['INFRAINVEST']));
+      .attr('width', d => this.x1(d['ROADACCID']));
+
+    newRow.insert('rect')
+      .attr('class','bar-infrainvest')
+      .attr('x', this.center + this.centerTextWidth / 2)
+      .attr('opacity', 0)
+      .attr('height', this.y.bandwidth())
+      .attr('width', d => this.x2(d['INFRAINVEST']));
 
     newRow.append('text')
       .attr('class','label')
       .attr('y', this.y.bandwidth() / 2)
-      .attr('x', d => this.x(d['INFRAINVEST']) + this.locationWidth)
+      .attr('x', d => this.x2(d['INFRAINVEST']) + this.centerTextWidth / 2)
       .attr('opacity', 0)
       .attr('dy','0.35em')
       .attr('dx','0.5em')
@@ -55,27 +66,32 @@ export class MainChart {
     
     newRow.append('text')
       .attr('class','location')
-      .attr('text-overflow','ellipsis')
+      .attr('text-anchor','middle')
       .attr('y', this.y.bandwidth() / 2)
-      .attr('x', 0)
+      .attr('x', this.center)
       .attr('opacity', 0)
       .attr('dy','0.35em')
-      .attr('dx','0.5em')
       .text(d => d['LOCATION']);
 
-    row.select('.bar')
+    row.select('.bar-roadaccid')
+      .attr('x', d => this.center - this.centerTextWidth / 2 - this.x1(d['ROADACCID']))
+      .attr('opacity', 1)
+      .attr('height', this.y.bandwidth())
+      .attr('width', d => this.x1(d['ROADACCID']))
+
+    row.select('.bar-infrainvest')
       .transition()
       .duration(300)
       .attr('height', this.y.bandwidth())
-      .attr('width', d => this.x(d['INFRAINVEST']))
+      .attr('width', d => this.x2(d['INFRAINVEST']))
       .attr('opacity', 1);
 
     row.select('.label')
       .transition()
       .duration(300)
       .attr('opacity', 1)
-      .attr('x', d => this.x(d['INFRAINVEST']) + this.locationWidth)
-      .text(d => d['INFRAINVEST']);
+      .attr('x', d => this.center + this.x2(d['INFRAINVEST']) + this.centerTextWidth / 2)
+      .text(d => `${d['INFRAINVEST']}‰ of GDP`);
 
     row.select('.location')
       .transition()
@@ -85,12 +101,15 @@ export class MainChart {
     row.exit()
       .transition()
       .style('opacity', 0)
-      .attr('transform', `translate(0, ${this.height + this.margin.top + this.margin.bottom})`)
+      .attr('transform', `translate(0, ${this.height})`)
       .remove();
 
     row.transition()
       .delay((d, i) => 200 + i * 30)
       .duration(900)
       .attr('transform', d => `translate(0, ${this.y(d['LOCATION'])})`);
+
+    row.select('.bar1')
+      .attr('y', d => this.y(d['LOCATION']) + this.y.bandwidth() / 2);
   };
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import * as d3 from 'd3';
+import { nest } from 'd3-collection';
 import throttle from 'lodash/throttle';
 import { sliderBottom } from 'd3-simple-slider'
 import { MainChart } from './charts';
 
 import './styles.scss';
-import { sortBy } from 'lodash';
 
 const USD_EUR_RATE = 0.83;
 const PRECISION = 2;
@@ -21,27 +21,45 @@ const PRECISION = 2;
     countryAlpha3Name[country['Alpha-3 code'].trim()] = country['Country'];
   });
 
-  const formatData = (year, sortBy) => {
-    const _data = data.filter(d => d['TIME'] === year);
-    return _data.map(y => ({
-      'LOCATION': countryAlpha3Name[y['LOCATION']],
-      'INFRAINVEST': +(y['INFRAINVEST'] / y['GDP'] * USD_EUR_RATE / 1000000 * 1000).toFixed(PRECISION),
-      'AIREMISSION': +(y['AIREMISSION']).toFixed(PRECISION) 
+  const formatData = (yearRange, sortBy) => {
+    const _data = data.filter(d => d['TIME'] >= yearRange[0] && d['TIME'] <= yearRange[1]);
+    const nestedData = nest()
+      .key(d => d['LOCATION'])
+      .rollup(v => ({ 
+        'INFRAINVEST': d3.mean(v, d => d['INFRAINVEST']),
+        'AIREMISSION': d3.mean(v, d => d['AIREMISSION']),
+        'PASSTRANSP': d3.mean(v, d => d['PASSTRANSP']),
+        'ROADACCID': d3.mean(v, d => d['ROADACCID']),
+        'GDP': d3.mean(v, d => d['GDP'])
+      }))
+      .entries(_data);
+
+    return nestedData.map(({ key, value }) => ({
+      'LOCATION': countryAlpha3Name[key],
+      'INFRAINVEST': +(value['INFRAINVEST'] / value['GDP'] * USD_EUR_RATE / 1000000 * 1000).toFixed(PRECISION),
+      'AIREMISSION': +(value['AIREMISSION']).toFixed(PRECISION),
+      'PASSTRANSP': +(value['PASSTRANSP']).toFixed(PRECISION),
+      'ROADACCID': +(value['ROADACCID']).toFixed(PRECISION)
     })).sort((a, b) => b[sortBy] - a[sortBy]);
   };
 
-  const mainChart = new MainChart(1024, 1024);
-  mainChart.update(formatData(2018, 'INFRAINVEST'));
+  const defaultDateRange = [2010, 2016];
+
+  const mainChart = new MainChart(512, 700);
+  mainChart.update(formatData(defaultDateRange, 'INFRAINVEST'));
+  mainChart.update(formatData(defaultDateRange, 'INFRAINVEST'));
 
   const sliderTime = sliderBottom()
     .min(d3.min(timeSet))
     .max(d3.max(timeSet))
-    .default(2018)
+    .default(defaultDateRange)
     .tickFormat(d3.format('d'))
     .tickValues([...timeSet])
     .step(1)
     .width(700)
+    .fill('skyblue')
     .on('onchange', throttle(val => {
+      mainChart.update(formatData(val, 'INFRAINVEST'));
       mainChart.update(formatData(val, 'INFRAINVEST'));
     }, 100));
 
